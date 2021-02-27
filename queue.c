0ebe2f932b3894989c6e94a5661d86c1f543edc7
hapion <hapionchen@gmail.com>
2021-02-27 18:08:58 +0800

    Implement q_new q_free q_insert_head q_insert_tail

--- queue.c
+++ queue.c
@@ -12,16 +12,25 @@
 queue_t *q_new()
 {
     queue_t *q = malloc(sizeof(queue_t));
-    /* TODO: What if malloc returned NULL? */
+    if (q == NULL)
+        return NULL;
     q->head = NULL;
+    q->tail = NULL;
+    q->num = 0;
     return q;
 }
 
 /* Free all storage used by queue */
 void q_free(queue_t *q)
 {
-    /* TODO: How about freeing the list elements and the strings? */
-    /* Free queue structure */
+    if (q == NULL)
+        return;
+    while (q->head != NULL) {
+        list_ele_t *tmp = q->head;
+        q->head = q->head->next;
+        free(tmp->value);
+        free(tmp);
+    }
     free(q);
 }
 
@@ -34,13 +43,25 @@ void q_free(queue_t *q)
  */
 bool q_insert_head(queue_t *q, char *s)
 {
+    if (!q)
+        return false;
+
     list_ele_t *newh;
-    /* TODO: What should you do if the q is NULL? */
     newh = malloc(sizeof(list_ele_t));
-    /* Don't forget to allocate space for the string and copy it */
-    /* What if either call to malloc returns NULL? */
+    if (newh == NULL)
+        return false;
+
+    char *tmp = malloc(sizeof(char) * (strlen(s) + 1));
+    if (tmp == NULL) {
+        free(newh);
+        return false;
+    }
+
+    newh->value = tmp;
+    strncpy(newh->value, s, (strlen(s) + 1));
     newh->next = q->head;
     q->head = newh;
+    q->num++;
     return true;
 }
 
@@ -53,10 +74,27 @@ bool q_insert_head(queue_t *q, char *s)
  */
 bool q_insert_tail(queue_t *q, char *s)
 {
-    /* TODO: You need to write the complete code for this function */
-    /* Remember: It should operate in O(1) time */
-    /* TODO: Remove the above comment when you are about to implement. */
-    return false;
+    if (q == NULL)
+        return false;
+    list_ele_t *newh;
+    newh = malloc(sizeof(list_ele_t));
+    if (newh == NULL)
+        return false;
+
+    char *tmp = malloc(sizeof(char) * (strlen(s) + 1));
+    if (tmp == NULL) {
+        free(newh);
+        return false;
+    }
+
+    newh->value = tmp;
+    strncpy(newh->value, s, (strlen(s) + 1));
+    q->tail->next = newh;
+    newh->next = NULL;
+    q->tail = newh;
+
+    q->num++;
+    return true;
 }
 
 /*
